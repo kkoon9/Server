f66a38089589f7d62875b48fe2ae3ee3d302c8ce
kkoon9 <rndrnjs2003@gmail.com>
2020-11-26 17:23:02 +0900

    feat: Health Check endpoints for ELB
    - ELB에 상태체크하는 라우터 생성

--- src/index.js
+++ src/index.js
@@ -25,5 +25,16 @@ log4js.configure({
 });
 logger.level = 'ALL';
 
+/**
+ * Health Check endpoints
+ * @TODO Explain why they are here
+ */
+app.get('/health', (req, res) => {
+  res.status(200).end();
+});
+app.head('/health', (req, res) => {
+  res.status(200).end();
+});
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
