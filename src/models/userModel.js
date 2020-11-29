22f226440ec7243e6402c7f95bdb9a9e1ea4de2a
kkoon9 <rndrnjs2003@gmail.com>
2020-11-29 21:22:36 +0900

    feat: seeder 생성
    - seeder : Dummy Data를 Import(Delete)해주는 파일

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please add an email'],
     match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
       'Please add a valid email'
     ]
   },
